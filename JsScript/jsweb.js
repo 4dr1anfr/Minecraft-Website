7b61cd72b28d46180b649420578f81e4888d94d9
4dr1anfr <adriandaph1@gmail.com>
2025-06-10 21:36:10 +0200

    image up

--- JsScript/jsweb.js
+++
@@ -1,8 +0,0 @@
-function copyIP_TAB2() {
-  const ipAddress = "play.smp-craft.de";
-  navigator.clipboard.writeText(ipAddress)
-    .then(() => {
-      document.getElementById("demo").innerText = "Copied the text: " + ipAddress;
-    })
-    .catch(err => console.error(err));
-}
